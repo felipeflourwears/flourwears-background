74d9932f9599d0fc008df77df0f11c2c0036997e
felipeflourwears <72992664+felipeflourwears@users.noreply.github.com>
2025-05-08 14:24:51 -0600

    SetUpt Environment

---
+++ README.md
@@ -0,0 +1,16 @@
+# Flourwears Background
+
+Interactive Site
+
+
+### Packages
+```bash
+tailwind
+npm install react-router-dom
+```
+
+
+### Icons
+```bash
+https://www.iconfinder.com/icons/1626634/stormtrooper_empire_star_wars_icon
+```
---
+++ src/components/Button.tsx
@@ -0,0 +1,42 @@
+import ButtonSvg from '../assets/svg/ButtonSvg';
+
+type ButtonProps = {
+  children: React.ReactNode;
+  className?: string;
+  href?: string;
+  px?: string;
+  white?: boolean;
+  onClick?: () => void;
+};
+
+const Button = ({ onClick, px, children, className = '', white, href=''}: ButtonProps) => {
+  
+    const classes = `button relative inline-flex items-center justify-center h-11 transition-colors hover:text-[var(--color-2)] ${
+        px || "px-7"
+      } ${white ? "text-[var(--n-8)]" : "text-[var(--n-1)]"} ${className || ""}`;      
+
+    const spanClasses = `relative z-10`;
+
+    const renderButton = () => (
+        <button
+        className={classes}
+        onClick={onClick}
+        >
+        <span className={spanClasses}>{children}</span>
+        {ButtonSvg(white)}
+        <h1>Hello Render Button</h1>
+        </button>
+    );
+
+    const renderLink = () => (
+        <a href={href} className={classes}>
+            <span className={spanClasses}>{children}</span>
+            {ButtonSvg(white)}
+            <h1>Hello Render Link</h1>
+        </a>
+    );
+
+  return href ? renderLink() : renderButton();
+};
+
+export default Button;
